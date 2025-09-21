fccba56419bf8a5e68e281ea46763cc4d19c8256
arqueon <arqueonautis@gmail.com>
2025-09-21 13:45:00 -0600

    lnk: sync configuration files

--- laptop.lnk/.config/niri/screenshot_swappy.sh
+++ laptop.lnk/.config/niri/screenshot_swappy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tmpfile=$(mktemp)
-grim -g "$(slurp)" - >"$tmpfile" && notify_view "swappy"
-swappy -f - <"$tmpfile"
+grim -g "$(slurp)" - >"$tmpfile" && notify_view "satty"
+satty -f - <"$tmpfile"
 rm "$tmpfile"
 
