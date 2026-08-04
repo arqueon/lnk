f0fdda1451db7a9163d660ad9aac06d9958a5043
Lnk User <lnk@localhost>
2026-08-04 17:10:27 -0600

    lnk: added launch-quad-agents2.sh

--- .config/niri/scripts/launch-quad-agents2.sh
+++ .config/niri/scripts/launch-quad-agents2.sh
@@ -1,3 +1,1 @@
-#!/usr/bin/env bash
-# Lanzador de matriz 2x2 (4 instancias) en workspace "agents2" (HDMI-A-1)
-exec "$HOME/.config/niri/scripts/launch-niri-matrix.sh" 4 agents2
+../../lnk/.config/niri/scripts/launch-quad-agents2.sh
