5636f15e2d2386ae22c9a165ba2106aa4391400d
Ruben Garcia <arqueonautis@gmail.com>
2026-08-11 11:29:11 -0600

    lnk: added lnk_pull_force.sh

--- .bin/lnk_pull_force.sh
+++ .bin/lnk_pull_force.sh
@@ -1,5 +1,1 @@
-#!/usr/bin/env bash
-LNK_REPO="${LNK_HOME:-$HOME/.config/lnk}"
-git -C "$LNK_REPO" fetch origin
-git -C "$LNK_REPO" reset --hard origin/main
-notify-send "lnk pull (force)" "$(lnk pull)"
+../.config/lnk/.bin/lnk_pull_force.sh
